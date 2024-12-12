992fe41bdc43076668cd9e9ba6c12b61701d3a66
Tai Chi Minh Ralph Eastwood <caffe@disroot.org>
2024-12-12 07:03:40 +0100

    wip

--- cuda/build.sh
+++ cuda/build.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
 
-COMMIT=$(git describe --long --dirty --abbrev=10 --tags --always)
-#podman build --target "image-full-builder" --build-arg="LIBC=glibc" --platform linux/amd64 . \
-#	--tag caffe3/void-builder:${COMMIT}
-#podman tag caffe3/void-builder:${COMMIT} caffe3/void-builder:latest
+CUDA_MAJOR=12
+CUDA_MINOR=6
+CUDA_REVISION=3
+UBUNTU_VERSION=24.04
 
-podman build \
-  --target "cuda-minimal-build" \
-  --build-arg="LIBC=glibc" \
-  --build-arg="CUDA_MAJOR=12" \
-  --build-arg="CUDA_MINOR=6" \
-  --build-arg="CUDA_REVISION=3" \
-  --build-arg="UBUNTU_VERSION=24.04" \
-  --platform linux/amd64 . \
-	--tag caffe3/cuda-minimal-build:12.6.3
-
-#podman build \
-#  --target "image-full-cuda-pytorch-ssh" \
-#  --build-arg="LIBC=glibc" \
-#  --build-arg="CUDA_MAJOR=12" \
-#  --build-arg="CUDA_MINOR=6" \
-#  --build-arg="CUDA_REVISION=3" \
-#  --build-arg="UBUNTU_VERSION=24.04" \
-#  --build-arg="PYTORCH_VERSION=2.6.0.dev20241209" \
-#  --platform linux/amd64 . \
-#	--tag caffe3/void-cuda-pytorch-nightly:12.6.3-2.6.0.dev20241209-${COMMIT}
+for artifact in cuda-minimal-build cuda-devel cuda-nsight-compute; do
+  podman build \
+    --target "$artifact" \
+    --build-arg="CUDA_MAJOR=$CUDA_MAJOR" \
+    --build-arg="CUDA_MINOR=$CUDA_MINOR" \
+    --build-arg="CUDA_REVISION=$CUDA_REVISION" \
+    --build-arg="UBUNTU_VERSION=$UBUNTU_VERSION" \
+    --platform linux/amd64 . \
+    --tag caffe3/$artifact:12.6.3
+done
